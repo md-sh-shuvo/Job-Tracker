15aeb64cc8f5089dca55bb1cb3a1689253b16bfd
md-sh-shuvo <shuvo.avail@gmail.com>
2026-02-23 03:27:49 +0600

    interview and rejected status update stype and functionality added

--- scripts/category.js
+++ scripts/category.js
@@ -6,38 +6,38 @@ const categoryRejected = document.getElementById('catagory-rejected')
 
 
 categoryAll.addEventListener("click",  function() {
-    categoryInterview.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryInterview.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryInterview.classList.add("bg-white", "hover:bg-[#3B82F6]", "hover:text-white")
 
 
-    categoryAll.classList.add("bg-[#3B82F6]", "text-white")
+    categoryAll.classList.add("bg-[#3B82F6]", "text-white", "active")
     categoryAll.classList.remove("bg-white")
 
-    categoryRejected.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryRejected.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryRejected.classList.add("bg-white")
 })
 
 
 categoryInterview.addEventListener("click", function() {
-    categoryAll.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryAll.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryAll.classList.add("bg-white", "hover:bg-[#3B82F6]", "hover:text-white")
 
-    categoryInterview.classList.add("bg-[#3B82F6]", "text-white")
+    categoryInterview.classList.add("bg-[#3B82F6]", "text-white", "active")
     categoryInterview.classList.remove("bg-white")
 
-    categoryRejected.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryRejected.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryRejected.classList.add("bg-white")
 })
 
 
 categoryRejected.addEventListener("click", function() {
 
-    categoryAll.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryAll.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryAll.classList.add("bg-white")
 
-    categoryInterview.classList.remove("bg-[#3B82F6]", "text-white")
+    categoryInterview.classList.remove("bg-[#3B82F6]", "text-white", "active")
     categoryInterview.classList.add("bg-white")
 
-    categoryRejected.classList.add("bg-[#3B82F6]", "text-white")
+    categoryRejected.classList.add("bg-[#3B82F6]", "text-white", "active")
     categoryRejected.classList.remove("bg-white")
 })
--- scripts/home.js
+++ scripts/home.js
@@ -106,8 +106,10 @@ let applicants = [
 const cardContainer = document.getElementById("card-container");
 
 document.getElementById("total-jobs").innerText = `${applicants.length} jobs`;
-document.getElementById("total-applicants-number").innerText =applicants.length;
+document.getElementById("total-applicants-number").innerText =
+  applicants.length;
 
+// for index (all category)
 applicants.forEach((applicant) => {
   const card = document.createElement("div");
   card.classList.add("p-6", "mb-4", "bg-white", "rounded-xl", "shadow-lg");
@@ -127,29 +129,24 @@ applicants.forEach((applicant) => {
       <p class="salary">&nbsp;&nbsp; •&nbsp;&nbsp; ${applicant.salary}</p>
     </div>
     <div class="py-5">
-      <p class="${applicant.status == "INTERVIEW" ? "text-green-600" : "text-red-500"} ${applicant.status ? "inline-block" : "hidden"}  py-2 px-3 mb-2 bg-[#EEF4FF] w-fit">
-        ${applicant.status}
+      <p class="applicant-status  hidden  py-2 px-3 mb-2 bg-[#EEF4FF] w-fit">
+        
       </p>
       <p class="text-[#323B49] text-sm">
         ${applicant.description} 
       </p>
     </div>
     <div class="flex gap-2">
-      <button class="px-3 py-2 rounded-sm font-semibold text-sm text-[#10B981] border-2 border-[#10B981] hover:bg-[#10B981] hover:text-white  active:scale-95">
-        interview
+      <button class="interview-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#10B981] border-2 border-[#10B981] hover:bg-[#10B981] hover:text-white  active:scale-95">
+        Interview
       </button>
-      <button class="px-3 py-2 rounded-sm font-semibold text-sm text-[#EF4444] border-2 border-[#EF4444] hover:bg-[#EF4444] hover:text-white active:scale-95">
+      <button class="rejected-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#EF4444] border-2 border-[#EF4444] hover:bg-[#EF4444] hover:text-white active:scale-95">
         Rejected
       </button>
     </div>
   `;
 
-
-  cardContainer.appendChild(card);
-
-  
-
-  card.querySelector(".dlt-btn").addEventListener("click", function (e) {
+  card.querySelector(".dlt-btn").addEventListener("click", function () {
     card.remove();
     applicants = applicants.filter((aplcnts) => aplcnts != applicant);
     console.log(applicants);
@@ -158,4 +155,264 @@ applicants.forEach((applicant) => {
     document.getElementById("total-applicants-number").innerText =
       applicants.length;
   });
+
+  card.querySelector(".interview-btn").addEventListener("click", function () {
+    applicant.status = "INTERVIEW";
+    applicantStatus = card.querySelector(".applicant-status");
+    applicantStatus.classList.remove("hidden", "text-red-500");
+    applicantStatus.classList.add("text-green-500");
+    applicantStatus.innerText = applicant.status;
+  });
+  card.querySelector(".rejected-btn").addEventListener("click", function () {
+    applicant.status = "REJECTED";
+    applicantStatus = card.querySelector(".applicant-status");
+    applicantStatus.classList.remove("hidden", "text-green-500");
+    applicantStatus.classList.add("text-red-500");
+    applicantStatus.innerText = applicant.status;
+  });
+
+  cardContainer.appendChild(card);
 });
+
+
+
+// for all category-->
+
+document.getElementById("catagory-all").addEventListener("click", function () {
+  cardContainer.innerHTML = "";
+
+  applicants.forEach((applicant) => {
+    const card = document.createElement("div");
+    card.classList.add("p-6", "mb-4", "bg-white", "rounded-xl", "shadow-lg");
+
+    card.innerHTML = `
+  <div class="flex justify-between items-center">
+  <h1 class="font-semibold text-[18px] text-[#002C5C]">${applicant.company}</h1>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 hover:text-red-500 cursor-pointer dlt-btn">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+  </div>
+  <p class="text-[#64748B] mb-5">${applicant.role}</p>
+  
+    <div class="flex text-[#64748B] text-sm">
+      <p class="type">${applicant.type}</p>
+      <p class="salary">&nbsp;&nbsp; •&nbsp;&nbsp; ${applicant.salary}</p>
+    </div>
+    <div class="py-5">
+      <p class="applicant-status ${applicant.status == "INTERVIEW" ? "text-green-500" : "text-red-500" } ${applicant.status ? "inline-block" : "hidden" } py-2 px-3 mb-2 bg-[#EEF4FF] w-fit">
+        ${applicant.status}
+      </p>
+      <p class="text-[#323B49] text-sm">
+        ${applicant.description} 
+      </p>
+    </div>
+    <div class="flex gap-2">
+      <button class="interview-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#10B981] border-2 border-[#10B981] hover:bg-[#10B981] hover:text-white  active:scale-95">
+        Interview
+      </button>
+      <button class="rejected-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#EF4444] border-2 border-[#EF4444] hover:bg-[#EF4444] hover:text-white active:scale-95">
+        Rejected
+      </button>
+    </div>
+  `;
+
+    card.querySelector(".dlt-btn").addEventListener("click", function () {
+      card.remove();
+      applicants = applicants.filter((aplcnts) => aplcnts != applicant);
+      console.log(applicants);
+      document.getElementById("total-jobs").innerText =
+        `${applicants.length} jobs`;
+      document.getElementById("total-applicants-number").innerText =
+        applicants.length;
+    });
+
+    card.querySelector(".interview-btn").addEventListener("click", function () {
+      applicant.status = "INTERVIEW";
+      applicantStatus = card.querySelector(".applicant-status");
+      applicantStatus.classList.remove("hidden", "text-red-500");
+      applicantStatus.classList.add("text-green-500");
+      applicantStatus.innerText = applicant.status;
+    });
+    card.querySelector(".rejected-btn").addEventListener("click", function () {
+      applicant.status = "REJECTED";
+      applicantStatus = card.querySelector(".applicant-status");
+      applicantStatus.classList.remove("hidden", "text-green-500");
+      applicantStatus.classList.add("text-red-500");
+      applicantStatus.innerText = applicant.status;
+    });
+
+    cardContainer.appendChild(card);
+  });
+});
+
+
+
+
+
+// for interviewed category-->
+
+document
+  .getElementById("catagory-interviewed")
+  .addEventListener("click", function () {
+    cardContainer.innerHTML = "";
+
+    const interviewedApplicants = applicants.filter(
+      (aplicant) => aplicant.status == "INTERVIEW",
+    );
+
+    // console.log(interviewedApplicants)
+
+    interviewedApplicants.forEach((applicant) => {
+      const card = document.createElement("div");
+      card.classList.add("p-6", "mb-4", "bg-white", "rounded-xl", "shadow-lg");
+
+      card.innerHTML = `
+  <div class="flex justify-between items-center">
+  <h1 class="font-semibold text-[18px] text-[#002C5C]">${applicant.company}</h1>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 hover:text-red-500 cursor-pointer dlt-btn">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+  </div>
+  <p class="text-[#64748B] mb-5">${applicant.role}</p>
+  
+    <div class="flex text-[#64748B] text-sm">
+      <p class="type">${applicant.type}</p>
+      <p class="salary">&nbsp;&nbsp; •&nbsp;&nbsp; ${applicant.salary}</p>
+    </div>
+    <div class="py-5">
+      <p class="applicant-status ${applicant.status == "INTERVIEW" ? "text-green-500" : "text-red-500" } ${applicant.status ? "inline-block" : "hidden"} py-2 px-3 mb-2 bg-[#EEF4FF] w-fit">
+        ${applicant.status}
+      </p>
+      <p class="text-[#323B49] text-sm">
+        ${applicant.description} 
+      </p>
+    </div>
+    <div class="flex gap-2">
+      <button class="interview-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#10B981] border-2 border-[#10B981] hover:bg-[#10B981] hover:text-white  active:scale-95">
+        Interview
+      </button>
+      <button class="rejected-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#EF4444] border-2 border-[#EF4444] hover:bg-[#EF4444] hover:text-white active:scale-95">
+        Rejected
+      </button>
+    </div>
+  `;
+
+      card.querySelector(".dlt-btn").addEventListener("click", function () {
+        card.remove();
+        applicants = applicants.filter((aplcnts) => aplcnts != applicant);
+        console.log(applicants);
+        document.getElementById("total-jobs").innerText =
+          `${applicants.length} jobs`;
+        document.getElementById("total-applicants-number").innerText =
+          applicants.length;
+      });
+
+      card
+        .querySelector(".interview-btn")
+        .addEventListener("click", function () {
+          applicant.status = "INTERVIEW";
+          applicantStatus = card.querySelector(".applicant-status");
+          applicantStatus.classList.remove("hidden", "text-red-500");
+          applicantStatus.classList.add("text-green-500");
+          applicantStatus.innerText = applicant.status;
+        });
+      card
+        .querySelector(".rejected-btn")
+        .addEventListener("click", function () {
+          applicant.status = "REJECTED";
+          applicantStatus = card.querySelector(".applicant-status");
+          applicantStatus.classList.remove("hidden", "text-green-500");
+          applicantStatus.classList.add("text-red-500");
+          applicantStatus.innerText = applicant.status;
+          card.remove()
+        });
+
+      cardContainer.appendChild(card);
+    });
+  });
+
+
+// for rejected category-->
+
+document
+  .getElementById("catagory-rejected")
+  .addEventListener("click", function () {
+    cardContainer.innerHTML = "";
+
+    const rejectedApplicants = applicants.filter(
+      (aplicant) => aplicant.status == "REJECTED",
+    );
+
+    // console.log(rejectedApplicants)
+
+    rejectedApplicants.forEach((applicant) => {
+      const card = document.createElement("div");
+      card.classList.add("p-6", "mb-4", "bg-white", "rounded-xl", "shadow-lg");
+
+      card.innerHTML = `
+  <div class="flex justify-between items-center">
+  <h1 class="font-semibold text-[18px] text-[#002C5C]">${applicant.company}</h1>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 hover:text-red-500 cursor-pointer dlt-btn">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+  </div>
+  <p class="text-[#64748B] mb-5">${applicant.role}</p>
+  
+    <div class="flex text-[#64748B] text-sm">
+      <p class="type">${applicant.type}</p>
+      <p class="salary">&nbsp;&nbsp; •&nbsp;&nbsp; ${applicant.salary}</p>
+    </div>
+    <div class="py-5">
+      <p class="applicant-status ${applicant.status == "INTERVIEW" ? "text-green-500" : "text-red-500" } ${applicant.status ? "inline-block" : "hidden"} py-2 px-3 mb-2 bg-[#EEF4FF] w-fit">
+        ${applicant.status}
+      </p>
+      <p class="text-[#323B49] text-sm">
+        ${applicant.description} 
+      </p>
+    </div>
+    <div class="flex gap-2">
+      <button class="interview-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#10B981] border-2 border-[#10B981] hover:bg-[#10B981] hover:text-white  active:scale-95">
+        Interview
+      </button>
+      <button class="rejected-btn px-3 py-2 rounded-sm font-semibold text-sm text-[#EF4444] border-2 border-[#EF4444] hover:bg-[#EF4444] hover:text-white active:scale-95">
+        Rejected
+      </button>
+    </div>
+  `;
+
+      card.querySelector(".dlt-btn").addEventListener("click", function () {
+        card.remove();
+        applicants = applicants.filter((aplcnts) => aplcnts != applicant);
+        console.log(applicants);
+        document.getElementById("total-jobs").innerText =
+          `${applicants.length} jobs`;
+        document.getElementById("total-applicants-number").innerText =
+          applicants.length;
+      });
+
+      card
+        .querySelector(".interview-btn")
+        .addEventListener("click", function () {
+          applicant.status = "INTERVIEW";
+          applicantStatus = card.querySelector(".applicant-status");
+          applicantStatus.classList.remove("hidden", "text-red-500");
+          applicantStatus.classList.add("text-green-500");
+          applicantStatus.innerText = applicant.status;
+          card.remove()
+        });
+      card
+        .querySelector(".rejected-btn")
+        .addEventListener("click", function () {
+          applicant.status = "REJECTED";
+          applicantStatus = card.querySelector(".applicant-status");
+          applicantStatus.classList.remove("hidden", "text-green-500");
+          applicantStatus.classList.add("text-red-500");
+          applicantStatus.innerText = applicant.status;
+        });
+
+      cardContainer.appendChild(card);
+    });
+  });
